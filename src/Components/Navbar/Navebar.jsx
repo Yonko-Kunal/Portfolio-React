a5a9adfedbb7f04f58a76487706b3574427b9ab5
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2024-09-25 18:36:29 +0530

    Navbar tsudo background added

--- src/Components/Navbar/Navebar.jsx
+++ src/Components/Navbar/Navebar.jsx
@@ -18,9 +18,9 @@ const Navebar = () => {
     }
 
     return (
-        <div className='Navbar'>
+        <nav className='Navbar'>
             <div className="nav-mob-open" onClick={openMenu}><i class="fa-solid fa-bars"></i></div>
-            <nav>
+            <div>
                 <ul ref={menuRef} className="nav-menu">
                     <div className='nav-mob-close' onClick={closeMenu}><i class="fa-solid fa-xmark" ></i></div>
                     <hr className='nav-line' />
@@ -30,14 +30,14 @@ const Navebar = () => {
                     <li><AnchorLink onClick={closeMenu} className='anchor-list' offset={50} href='#contactId'><p>Contact</p></AnchorLink></li><hr className='nav-line' />
                 </ul>
 
-            </nav>
+            </div>
 
             <div className='nav-right'>
                 <div className="git-logo"><i onClick={gitLinkFunction} class="fa-brands fa-github"></i></div>
                 <div className="nav-connect"><AnchorLink className='anchor-list' offset={50} href='#contactId'>Connect With Me</AnchorLink></div>
             </div>
 
-        </div>
+        </nav>
     )
 }
 
